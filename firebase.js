6fcb61806086716ea697cfd3f0edd3926400e919
Omer Dolev <odolev@microsoft.com>
2022-09-28 01:05:58 +0300

    add auth

--- firebase.js
+++ firebase.js
@@ -1,6 +1,7 @@
 // Import the functions you need from the SDKs you need
 import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
 import { getFirestore, collection, getDocs } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js";
+import { getAuth, onAuthStateChanged, GoogleAuthProvider, signInWithRedirect, getRedirectResult } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
 // TODO: Add SDKs for Firebase products that you want to use
 // https://firebase.google.com/docs/web/setup#available-libraries
 
@@ -17,5 +18,18 @@ const firebaseConfig = {
 // Initialize Firebase
 const app = initializeApp(firebaseConfig);
 const db = getFirestore(app);
+
+const auth = getAuth();
+const authProvider  = new GoogleAuthProvider();
+
+async function login() {
+    const res = await getRedirectResult(auth);
+
+    if (!res) await signInWithRedirect(auth, authProvider);
+}
+
+onAuthStateChanged(auth, user => {
+    !user && login();
+});
 
 export { app, db, collection, getDocs };
