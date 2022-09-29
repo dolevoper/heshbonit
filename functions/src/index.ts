fbc6975b24dbcc799472cdfd8e4eee91c6c86cf5
Omer Dolev <odolev@microsoft.com>
2022-09-29 15:50:16 +0300

    wip

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,8 +1,11 @@
+import * as path from "path";
+import {tmpdir} from "os";
+import {unlink} from "fs/promises";
+import * as logger from "firebase-functions/logger";
 import * as functions from "firebase-functions";
 import {initializeApp, applicationDefault} from "firebase-admin/app";
 import {getStorage} from "firebase-admin/storage";
 import {generatePdf} from "html-pdf-node";
-import {unlink} from "fs/promises";
 
 initializeApp({
   credential: applicationDefault(),
@@ -13,10 +16,20 @@ export const createInvoice =
         .firestore
         .document("invoices/{id}")
         .onCreate(async (snap) => {
-          await generatePdf(
-              {content: "<h1>hello world</h1>"},
-              {format: "A4", path: `${snap.id}.pdf`}
-          );
-          await getStorage().bucket().upload(`${snap.id}.pdf`);
-          await unlink(`${snap.id}.pdf`);
+          try {
+            logger.info("starting invoice creation", snap.id);
+            const filedir = path.join(tmpdir(), `${snap.id}.pdf`);
+            await generatePdf(
+                {content: "<h1>hello world</h1>"},
+                {format: "A4", path: filedir}
+            );
+            logger.debug("created pdf file successfully");
+            await getStorage().bucket().upload(filedir);
+            logger.debug("uploaded pdf to storage");
+            await unlink(filedir);
+            logger.debug("deleted temp file successfully");
+            logger.info("invoice created");
+          } catch (err) {
+            logger.error(err);
+          }
         });
