9c147010acd91e0575f20a808b379bdb618162d3
Omer Dolev <odolev@microsoft.com>
2022-09-29 10:36:25 +0300

    wip

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,9 +1,22 @@
 import * as functions from "firebase-functions";
+import {initializeApp, applicationDefault} from "firebase-admin/app";
+import {getStorage} from "firebase-admin/storage";
+import {generatePdf} from "html-pdf-node";
+import {unlink} from "fs/promises";
 
-// // Start writing Firebase Functions
-// // https://firebase.google.com/docs/functions/typescript
-//
-// export const helloWorld = functions.https.onRequest((request, response) => {
-//   functions.logger.info("Hello logs!", {structuredData: true});
-//   response.send("Hello from Firebase!");
-// });
+initializeApp({
+  credential: applicationDefault(),
+});
+
+export const createInvoice =
+    functions
+        .firestore
+        .document("invoices/{id}")
+        .onCreate(async (snap) => {
+          await generatePdf(
+              {content: "<h1>hello world</h1>"},
+              {format: "A4", path: `${snap.id}.pdf`}
+          );
+          await getStorage().bucket().upload(`${snap.id}.pdf`);
+          await unlink(`${snap.id}.pdf`);
+        });
