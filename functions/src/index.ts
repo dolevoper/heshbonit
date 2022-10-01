818063955ecc9967750dfadb9c5b117bf248150a
Omer Dolev <odolev@microsoft.com>
2022-10-01 22:08:16 +0300

    create pdf on invoice creation

--- functions/src/index.ts
+++ functions/src/index.ts
@@ -1,35 +1,44 @@
-import * as path from "path";
-import {tmpdir} from "os";
-import {unlink} from "fs/promises";
 import * as logger from "firebase-functions/logger";
 import * as functions from "firebase-functions";
 import {initializeApp, applicationDefault} from "firebase-admin/app";
 import {getStorage} from "firebase-admin/storage";
-import {generatePdf} from "html-pdf-node";
+import * as PDFDoc from "pdfkit";
 
 initializeApp({
   credential: applicationDefault(),
 });
 
 export const createInvoice =
-    functions
-        .firestore
-        .document("invoices/{id}")
-        .onCreate(async (snap) => {
-          try {
-            logger.info("starting invoice creation", snap.id);
-            const filedir = path.join(tmpdir(), `${snap.id}.pdf`);
-            await generatePdf(
-                {content: "<h1>hello world</h1>"},
-                {format: "A4", path: filedir}
-            );
-            logger.debug("created pdf file successfully");
-            await getStorage().bucket().upload(filedir);
-            logger.debug("uploaded pdf to storage");
-            await unlink(filedir);
-            logger.debug("deleted temp file successfully");
-            logger.info("invoice created");
-          } catch (err) {
-            logger.error(err);
-          }
-        });
+  functions
+      .firestore
+      .document("invoices/{id}")
+      .onCreate(async (snap) => {
+        try {
+          logger.info("starting invoice creation", snap.id);
+
+          const file = getStorage()
+              .bucket("heshbonit-invoices")
+              .file(`${snap.id}.pdf`);
+          const doc = new PDFDoc();
+
+          await new Promise((resolve, reject) => {
+            const stream = file.createWriteStream({
+              resumable: false,
+              contentType: "application/pdf",
+            });
+
+            stream.on("finish", resolve);
+            stream.on("error", reject);
+
+            doc.pipe(stream);
+
+            doc.text("hello world");
+
+            doc.end();
+          });
+
+          logger.info("invoice created");
+        } catch (err) {
+          logger.error(err);
+        }
+      });
